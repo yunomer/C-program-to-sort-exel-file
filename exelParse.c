72b0ebdf2b586db9eff3d97a23ad7b6dc43985fe
socs <socs@socsvm>
2017-03-01 20:06:57 -0500

    Need to fix the parser

--- exelParse.c
+++ exelParse.c
@@ -17,9 +17,10 @@ int main (int argc, char *argv[])
     char fileName[255];
     int size = 0;
     char *theToken;
-    int c, i, j;
+    int i, j;
     int numRows = 0;
     int numCols = 0;
+    char *temp;
     char current_char = '*';
     char to_replace = '\n';
     char replacement = ' ';
@@ -34,40 +35,52 @@ int main (int argc, char *argv[])
         size = ((ftell(filePtr)) + 2); //Storing the file size in variable
         rewind(filePtr); //Rewinding back to the start of the file
     char string[size];
-  //      printf("Please enter the number of Rows\n"); //Asking user for the numRows to create array
-    //    scanf("%d", &numRows); //Asking for COls now to create the 3Darray
-   //     printf("Enter the number of Cols\n");
-  //      scanf(" %d", &numCols);
-        while ((current_char == fgetc(filePtr)) != EOF) {
+        printf("Please enter the number of Rows\n"); //Asking user for the numRows to create array
+        scanf("%d", &numRows); //Asking for COls now to create the 3Darray
+        printf("Enter the number of Cols\n");
+        scanf(" %d", &numCols);
+        while ((current_char = fgetc(filePtr)) != EOF) {
             if(current_char == to_replace) {
                 fseek(filePtr, ftell(filePtr)-1, SEEK_SET);
                 fprintf(filePtr, "%c", replacement);
-                printf("me here\n");
             }
         }
-        fgets(string[size], size, filePtr);
+        fseek(filePtr, -2, SEEK_END); //Insert the newline char at the end again.
+        fprintf(filePtr, "%c", to_replace);
+        rewind(filePtr); //Go back to the start of the stream
+        fgets(string, size, filePtr);
         printf("%s\n", string);
-/*
+          /*It works up till here*/
+
     char array[numRows][numCols][50]; //Creating the 3D array
-        theToken = strtok(string, " ,");
-        if(theToken = NULL) {
-            theToken = '0';
-            strcpy(array[0][0][50], theToken);
+        theToken = strtok(string, ",");
+        while (theToken != NULL) {
+            printf("%s ", theToken);
+            theToken = strtok(NULL, ",");
+            }
         }
-        else {
-            strcpy(array[0][0][50], theToken);
-        }
-        for(i=1; i<numRows; i++) {
+
+/*
+        theToken = strtok(string, ",");
+        for(i=0; i<numRows; i++) {
             for(j=1; j<numCols; j++) {
-                theToken = strtok(NULL, " ,");
+                theToken = strtok(NULL, ",");
+                temp = &array[i][j][50]; //pointer
                 if (theToken != NULL) {
-                    strcpy(array[i][j][50], theToken);
+                    strcpy(temp, theToken);
                 }
                 else {
-                    strcpy(array[i][j][50], "0");
+                    strcpy(temp, "0");
                 }
             }
-        }*/
+        }
+*/
+/*        for(i=0; i<numRows; i++) { //This should print the array
+            for(j=0; j<numCols; j++) {
+                printf("%s ", array[i][j][50]);
+            }
+        }
+*/
     fclose(filePtr);
     return 0;
 }
@@ -76,7 +89,6 @@ FILE *filePointer(char fileName[], char *mode)
 {
     FILE *tempFile = NULL;
     char tempFile2[255];
-    char tempName[255];
         strcpy(tempFile2, fileName);
         tempFile = fopen(tempFile2, mode);
         if (tempFile == NULL) {
